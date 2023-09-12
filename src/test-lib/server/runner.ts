df12570dd1c6d45c015ec804a2295543977d8a7d
mizuki_r <ry.mizuki@gmail.com>
2023-09-12 09:20:30 +0900

    refactor(test-lib/server): Organize event handling on runner

--- src/test-lib/server/runner.ts
+++ src/test-lib/server/runner.ts
@@ -1,11 +1,13 @@
-import { ChildProcess, fork } from 'child_process'
+import { ChildProcess, Serializable, fork } from 'child_process'
 import { join } from 'path'
+import { EventEmitter } from 'stream'
+
 
 export type ServerOptions = {
   serverPath?: string
 }
 
-export class Server {
+export class Server extends EventEmitter {
   private options: ServerOptions
   private process: ChildProcess | null = null
   private server: {
@@ -14,6 +16,7 @@ export class Server {
   } | null = null
 
   constructor(options: ServerOptions = {}) {
+    super()
     this.options = options
   }
 
@@ -21,10 +24,15 @@ export class Server {
     return `http://${this.server.host}:${this.server.port}`
   }
 
-  async listen(port = 0, host?: string): Promise<void> {
+  async listen(
+    port = 0,
+    host?: string
+  ): Promise<{ port: number; host: string }> {
     const source = this.options.serverPath || join(__dirname, './server.js')
 
     this.process = fork(source)
+    this.process.on('message', (message) => this.onMessage(message))
+
     this.process.send(
       JSON.stringify({
         type: 'listen',
@@ -35,14 +43,9 @@ export class Server {
       }
     )
 
-    return new Promise((resolve) => {
-      this.process.on('message', (message) => {
-        // eslint-disable-next-line
-        const { payload } = JSON.parse(message.toString()) as any
-        // eslint-disable-next-line
-        const { port, host } = payload as { host: string; port: number }
-        this.server = { port, host }
-        resolve(undefined)
+    return await new Promise((resolve) => {
+      this.once('listen', () => {
+        resolve({ port: this.server.port, host: this.server.host })
       })
     })
   }
@@ -58,6 +61,23 @@ export class Server {
       this.process.kill('SIGHUP')
       this.process = null
     })
+  }
+
+  private onMessage(message: Serializable) {
+    if (typeof message !== 'string') {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      throw new Error(`"${message as any}" can not be parse`)
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { type, payload } = JSON.parse(message)
+    switch (type) {
+      case 'listen': {
+        const { port, host } = payload as { host: string; port: number }
+        this.server = { port, host }
+        this.emit('listen')
+        break
+      }
+    }
   }
 }
 
